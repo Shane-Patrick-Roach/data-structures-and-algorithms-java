c5125adac19bbbc5d3d023ca85d94cdc7a72e782
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-02-18 22:30:17 -0800

    created file but could not get my intellij to work

---
+++ datastructures/lib/src/test/java/datastructures/LinkedListTest.java
@@ -0,0 +1,15 @@
+package datastructures.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class TestLinkedList {
+
+    @Test
+    void testRolls() {
+        LinkedList sut = new LinkedList();
+        System.out.println(sut);
+    }
+}
