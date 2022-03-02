f90c4ab912e4ca856cd49058d9e8aa20c8788a9e
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-03-02 08:41:50 -0800

    finished animal shelter

--- linked-list/codechallengesREADME/README.codechallenge12.md
+++ linked-list/codechallengesREADME/README.codechallenge12.md
@@ -11,4 +11,20 @@ The big(O) space efficiency would also be O(1) because there is no more space be
 
 ## Solution
 
-WIP.
+### Enqueue
+The approach I took was to use two different stacks. Stack 1 was the considered the shuffler and stack 2 was the container.
+Every time a pet is enqueued into the shelter, the stack2 is inserted into stack 1 and then the new pet is added at the top of the stack.
+Then the elements in stack1 are emptied back into stack2 to make sure that the top of stack 2 was the first element enqueued.
+
+### Dequeue
+The approach for dequeue was a little more complex because we had to filter if the String argument specified a certain cat or dog.
+The algorithm would check to see if the top of the stack 2 was equal to the preferred type, if it is it would return that animal object.
+If not, then the element would be added into stack 1 and check the next element in stack2. Once an element with the preffered type was found,
+it would pop() that element off the stack2, and then read all the elements placed in stack1 back into stack 2, this maintaining FIFO.
+
+- Test `testAnimalShelterEnqueue` tests to enqueue an animal object.
+- Test `testAnimalShelterDequeueFirstCat` tests to dequeue the first "cat" placed into the shelter.
+- Test `testAnimalShelterDequeueFirstDog` tests to dequeue the first "dog" placed into the shelter.
+- Test `testAnimalShelterDequeueMultipleDogs` tests to dequeue multiple dogs placed into the shelter.
+- Test `testAnimalShelterDequeueReturnNull` tests if a value is null is returned if a preferred type is not specified as "cat" or "dog".
+
--- linked-list/lib/src/test/java/codechallenges/animalShelter/AnimalShelterTest.java
+++ linked-list/lib/src/test/java/codechallenges/animalShelter/AnimalShelterTest.java
@@ -11,21 +11,75 @@ public class AnimalShelterTest {
     @Test
     void testAnimalShelterEnqueue() throws Exception {
         AnimalShelter sut = new AnimalShelter();
-        Animal Ben = new Animal("dog");
-        Animal Mike = new Animal("dog");
-        Animal Sarah = new Animal("cat");
-        Animal Sally = new Animal("cat");
+        Animal Ben = new Animal("dog", "ben");
+        Animal Mike = new Animal("dog", "mike");
+        Animal Sarah = new Animal("cat", "sarah");
+        Animal Sally = new Animal("cat", "sally");
         sut.enqueue(Sally);
         sut.enqueue(Ben);
         sut.enqueue(Mike);
         sut.enqueue(Sarah);
-        sut.dequeue("cat");
+        assert(sut.stack2.size == 4);
+    }
+
+
+    @Test
+    void testAnimalShelterDequeueFirstCat() throws Exception {
+        AnimalShelter sut = new AnimalShelter();
+        Animal Ben = new Animal("dog", "ben");
+        Animal Mike = new Animal("dog", "mike");
+        Animal Sarah = new Animal("cat", "sarah");
+        Animal Sally = new Animal("cat", "sally");
+        sut.enqueue(Sally);
+        sut.enqueue(Ben);
+        sut.enqueue(Mike);
+        sut.enqueue(Sarah);
+        assert(sut.dequeue("cat").toString().equals("Animal{type='cat', name='sally'}"));
+    }
+
+
+    @Test
+    void testAnimalShelterDequeueFirstDog() throws Exception {
+        AnimalShelter sut = new AnimalShelter();
+        Animal Ben = new Animal("dog", "ben");
+        Animal Mike = new Animal("dog", "mike");
+        Animal Sarah = new Animal("cat", "sarah");
+        Animal Sally = new Animal("cat", "sally");
+        sut.enqueue(Sally);
+        sut.enqueue(Ben);
+        sut.enqueue(Mike);
+        sut.enqueue(Sarah);
+        assert(sut.dequeue("dog").toString().equals("Animal{type='dog', name='ben'}"));
+    }
+
+    @Test
+    void testAnimalShelterDequeueMultipleDogs() throws Exception {
+        AnimalShelter sut = new AnimalShelter();
+        Animal Ben = new Animal("dog", "ben");
+        Animal Mike = new Animal("dog", "mike");
+        Animal Sarah = new Animal("cat", "sarah");
+        Animal Sally = new Animal("cat", "sally");
+        sut.enqueue(Sally);
+        sut.enqueue(Ben);
+        sut.enqueue(Mike);
+        sut.enqueue(Sarah);
         sut.dequeue("dog");
-        sut.dequeue("dog");
-        System.out.println(sut.stack2.top.value.type);
-        System.out.println(sut.stack2.size);
+        assert(sut.dequeue("dog").toString().equals("Animal{type='dog', name='mike'}"));
     }
 
 
 
+    @Test
+    void testAnimalShelterDequeueReturnNull() throws Exception {
+        AnimalShelter sut = new AnimalShelter();
+        Animal Ben = new Animal("dog", "ben");
+        Animal Mike = new Animal("dog", "mike");
+        Animal Sarah = new Animal("cat", "sarah");
+        Animal Sally = new Animal("cat", "sally");
+        sut.enqueue(Sally);
+        sut.enqueue(Ben);
+        sut.enqueue(Mike);
+        sut.enqueue(Sarah);
+        assert(sut.dequeue("lizard") == null);
+    }
 }
