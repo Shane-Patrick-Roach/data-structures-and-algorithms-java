7ea484615b95e7a863d8bf51bb0ddcc48d28001f
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-03-04 18:04:47 -0800

    started trees code challenge

---
+++ linked-list/lib/src/main/java/datastructures/tree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+//package datastructures.tree;
+//
+//public class BinarySearchTree extends BinaryTree{
+//
+//    public void add(T value){
+//
+//    }
+//
+//    public boolean contains(T value){
+//        return false;
+//    }
+//}
---
+++ linked-list/lib/src/main/java/datastructures/tree/Node.java
@@ -0,0 +1,32 @@
+package datastructures.tree;
+
+public class Node {
+    int value;
+    Node leftNode;
+    Node rightNode;
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+}
