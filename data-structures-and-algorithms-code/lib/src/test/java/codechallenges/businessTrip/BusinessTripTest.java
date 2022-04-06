21e8df07731402c98000cf33224931c4a4b4a556
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-04-05 20:42:53 -0700

    commented out WIP code

--- data-structures-and-algorithms-code/lib/src/test/java/codechallenges/businessTrip/BusinessTripTest.java
+++ data-structures-and-algorithms-code/lib/src/test/java/codechallenges/businessTrip/BusinessTripTest.java
@@ -1,42 +1,42 @@
-package codechallenges.businessTrip;
-
-import datastructures.graph.Graph;
-import datastructures.graph.Vertex;
-import org.junit.jupiter.api.Test;
-
-import java.sql.Array;
-import java.util.ArrayList;
-
-import static codechallenges.graphbusinesstrip.BusinessTrip.businessTrip;
-
-public class BusinessTripTest {
-
-    @Test
-    void testGraphBreathFirstTraversalShortestGraph() throws Exception {
-
-//            A
-//        /       \
-//       E         B
+//package codechallenges.businessTrip;
 //
-        Graph<String> sut = new Graph<>(10);
-        Vertex<String> vertexA = sut.addVertex("A");
-        Vertex<String> vertexB = sut.addVertex("B");
-        Vertex<String> vertexE = sut.addVertex("E");
-
-
-
-
-
-        sut.addEdge(vertexA,vertexB);
-        sut.addEdge(vertexA,vertexE);
-
-        String[] input = new String[]{"A","B"};
-
-
-        businessTrip(sut, input);
-
-    }
-
-
-
-}
+//import datastructures.graph.Graph;
+//import datastructures.graph.Vertex;
+//import org.junit.jupiter.api.Test;
+//
+//import java.sql.Array;
+//import java.util.ArrayList;
+//
+//import static codechallenges.graphbusinesstrip.BusinessTrip.businessTrip;
+//
+//public class BusinessTripTest {
+//
+//    @Test
+//    void testGraphBreathFirstTraversalShortestGraph() throws Exception {
+//
+////            A
+////        /       \
+////       E         B
+////
+//        Graph<String> sut = new Graph<>(10);
+//        Vertex<String> vertexA = sut.addVertex("A");
+//        Vertex<String> vertexB = sut.addVertex("B");
+//        Vertex<String> vertexE = sut.addVertex("E");
+//
+//
+//
+//
+//
+//        sut.addEdge(vertexA,vertexB);
+//        sut.addEdge(vertexA,vertexE);
+//
+//        String[] input = new String[]{"A","B"};
+//
+//
+//        businessTrip(sut, input);
+//
+//    }
+//
+//
+//
+//}
